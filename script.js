ac50f5ab4b53688b8e536a70b25ffc0a8a82d089
stutzro <35533654+stutzro@users.noreply.github.com>
2019-02-26 13:37:56 -0700

    Added initial cv website files

---
+++ script.js
@@ -0,0 +1,4 @@
+window.onload = function(){
+/*	document.querySelectorAll('results' 'answer1' 'answer2' 'answer3' ).style.display = 'none';*/
+  document.getElementById('results' ).style.display = 'none';
+}
